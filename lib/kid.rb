138c1dbc3a111286f0ce4ee58172febaedf48988
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-07-21 19:19:10 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid 
+  include dance 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end 
   
   
   
